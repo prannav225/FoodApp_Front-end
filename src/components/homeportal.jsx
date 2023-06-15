891594a85b12a8a23a182dd3ba8b043724f2225b
prannav225 <121098142+prannav225@users.noreply.github.com>
2023-06-15 13:23:38 +0530

    added error component

---
+++ src/components/homeportal.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
+import Posts from "./posts";
+import Food from "./food";
+import SinglePost from "./singlePost";
+import CollapsibleExample from "./navbar";
+import Footer from "./footer";
+import Error from "./Error";
+
+const HomePortal = () => {
+  return (
+    <div className="homeportal">
+      <CollapsibleExample />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/addposts" element={<Posts />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/food/:id" element={<SinglePost />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePortal;
